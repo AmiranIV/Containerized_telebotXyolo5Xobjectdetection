dcef102d035a842a7fbc5145f9a162d839fdebe9
Amiran Ivgi <109898333+AmiranIV@users.noreply.github.com>
2023-09-26 14:15:25 +0300

    Update new-start-app.sh
    fix

--- new-start-app.sh
+++ new-start-app.sh
@@ -5,6 +5,8 @@ sleep 3
 echo "Docker compose up..."
 docker compose up
 sleep 5
+bash rs-init.sh
+sleep 5
 echo "Overwriting secrets.env..."
-echo "TELEGRAM_TOKEN=<USE YOUR TELEGRAM TOKEN HERE> " > secrets.env
+echo "TELEGRAM_TOKEN=<USE YOUR TELEGRAM TOKEN HERE> " > /home/ubuntu/secrets.env
 echo "TELEGRAM_APP_URL=<replacethis>" >> /home/ubuntu/secrets.env  ##DON't TOUCH THE <replacethis>
